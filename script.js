981577963af66518845d3939dcbf47daa4e2964c
Elaine Shaw <57796267+elshaw82@users.noreply.github.com>
2020-06-13 22:45:01 +0100

    Basic set up

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.querySelector(".button");
 button.addEventListener('click', confettiDrop);
 
 function confettiDrop() {
-  var inner = document.querySelectorAll("inner");
-    inner.classList.add("confetti");
-}  
+console.log("You clicked me for confetti!");
+}
+
+
