c83d736da07057e1f5bf730c0924603c7490dcb6
Elaine Shaw <57796267+elshaw82@users.noreply.github.com>
2020-06-14 16:24:04 +0100

    Complete

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// grab the button
 const button = document.querySelector(".button");
 button.addEventListener('click', confettiDrop);
 
 function confettiDrop() {
-console.log("You clicked me for confetti!");
+    //grab the divs
+var divs = document.querySelectorAll(".inner");
+// loop over and add the new class
+ for (var i = 0; i < divs.length; i++) {
+     divs[i].classList.add("confetti");
+ }
 }
-
-
